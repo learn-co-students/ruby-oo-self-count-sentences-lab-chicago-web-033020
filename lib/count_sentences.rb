350509fdaac33cc35fb37e7b2f2b2e89f2f0e496
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-01 23:43:12 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,28 @@ require 'pry'
 
 class String
 
+  # attr_accessor :str
+
+  # def initialize(str)
+  #   @str = str
+  # end
+
   def sentence?
-    
+    self.end_with?('.') ? true : false
   end
 
   def question?
-
+    self.end_with?('?') ? true : false
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+# my_string = StringChecker.new("hello, my name is isaac.")
+# binding.pry
